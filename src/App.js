44a70f590951e7a41effda313922d3225ab7ae2f
JoachimBjerkland <111729453+JoachimBjerkland@users.noreply.github.com>
2023-03-13 12:16:12 +0100

    Arbeidskrav 4

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import MovieCard from './components/MovieCard';
+import SearchResults from './components/SearchResults';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/movie/:id" element={<MovieCard />} />
+      <Route path="/search" element={<SearchResults />} />
+    </Routes>
+  );
+}
+
+export default App;
