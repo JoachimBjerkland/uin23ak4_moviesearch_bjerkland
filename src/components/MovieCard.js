524713fb4c1034f3bf0257bd709b0400491d1d5b
JoachimBjerkland <111729453+JoachimBjerkland@users.noreply.github.com>
2023-03-19 15:05:09 +0100

    Arbeidskrav 4

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -5,15 +5,21 @@ function MovieCard(props) {
   return (
     <div>
       <h2>{movie.Title}</h2>
-      <div>
-        <img src={movie.Poster} alt={movie.Title} />
-      </div>
+      {movie.Poster !== 'N/A' ? (
+        <div>
+          <img src={movie.Poster} alt={movie.Title} />
+        </div>
+      ) : (
+        <div>No poster available</div>
+        //Ettersom det er ihvertfall 1 film uten poster, så er denne lagt til for å vise frem dette.
+      )}
       <ul>
-        {Object.entries(movie).map(([key, value]) => (
-          <li key={key}>
-            <strong>{key}:</strong> {value}
-          </li>
-        ))}
+        <li>
+          <strong>Year:</strong> {movie.Year}
+        </li>
+        <li>
+          <strong>Type:</strong> {movie.Type}
+        </li>
       </ul>
     </div>
   );
